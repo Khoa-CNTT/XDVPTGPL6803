3bfa48fa6030cee4425e23f04d561bba0a45bd3a
HieuD <63394751+conghieeu@users.noreply.github.com>
2025-04-05 21:52:10 +0700

    project

--- KLTNLongKhoi/Assets/_External/MyMechanics/Culling/2_Scripts/LevelLoader.cs
+++ KLTNLongKhoi/Assets/_External/MyMechanics/Culling/2_Scripts/LevelLoader.cs
@@ -4,33 +4,8 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
-namespace HieuDev
+/// <summary> Hiện ứng chuyển cảnh </summary>
+public class LevelLoader : MonoBehaviour
 {
-    /// <summary> Hiện ứng chuyển cảnh </summary>
-    public class TransEffect : MonoBehaviour
-    {
-        [SerializeField] Animator _anim;
-        [SerializeField] string _nextSceneName;
-        [SerializeField] float transitionTime = 1f;
 
-        public void LoadNextLevel(string nextSceneName)
-        {
-            _nextSceneName = nextSceneName;
-            LoadNextLevel();
-        }
-
-        public void LoadNextLevel()
-        {
-            StartCoroutine(LoadLevel(_nextSceneName));
-        }
-
-        IEnumerator LoadLevel(String sceneName)
-        {
-            _anim.SetTrigger("Start");
-            yield return new WaitForSeconds(transitionTime);
-            SceneManager.LoadScene(sceneName);
-        }
-
-
-    }
 }
